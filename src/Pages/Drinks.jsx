186c99f654ac758c5b03b0a477508f349beb9896
Aline Bury <alinebury@gmail.com>
2022-04-26 19:20:45 -0300

    Header requisito 9

---
+++ src/Pages/Drinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+class Drinks extends React.Component {
+  render() {
+    return (
+      <Header title="Drinks" />
+    );
+  }
+}
+
+export default Drinks;
---
+++ src/Pages/Foods.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+class Foods extends React.Component {
+  render() {
+    return (
+      <Header title="Foods" />
+    );
+  }
+}
+
+export default Foods;
