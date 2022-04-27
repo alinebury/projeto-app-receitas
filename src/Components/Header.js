b173cbd94503a4dc42b67f526f3115e9c1b6df01
ClawNarok <arthurvgf@gmail.com>
2022-04-27 18:07:31 -0300

    [Main group 4] Correção css e context +css para Footer +css Body *css Header *Conversão de react para context

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,55 +1,47 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import RecipesContext from '../Context/RecipesContext';
+import '../Styles/Header.css';
 import searchIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
 
-export default class Header extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showSearchBar: false,
-    };
-  }
+function Header() {
+  const { title } = useContext(RecipesContext);
+  const [showSearchBar, setShowSearchBar] = useState(false);
 
-  handleCLick = () => {
-    const { showSearchBar } = this.state;
-    this.setState({ showSearchBar: !showSearchBar });
-  }
-
-  render() {
-    const { showSearchBar } = this.state;
-    return (
-      <header>
-        <nav>
-          <button
-            type="button"
-            data-testid="search-top-btn"
+  return (
+    <header>
+      <nav>
+        <button
+          type="button"
+          data-testid="search-top-btn"
+          src={ searchIcon }
+          onClick={ () => setShowSearchBar((prevState) => !prevState) }
+        >
+          <img
             src={ searchIcon }
-            onClick={ this.handleCLick }
-          >
-            <img
-              src={ searchIcon }
-              alt="search icon"
-            />
-          </button>
-          <h2 data-testid="page-title">Foods</h2>
-          <Link to="/profile">
-            <img
-              data-testid="profile-top-btn"
-              src={ profileIcon }
-              alt="profile icon"
-            />
-          </Link>
-        </nav>
-
-        {
-          showSearchBar && <input
-            data-testid="search-input"
-            type="search"
-            placeholder="pesquisar receita"
+            alt="search icon"
           />
-        }
-      </header>
-    );
-  }
+        </button>
+        <h2 data-testid="page-title">{ title }</h2>
+        <Link to="/profile">
+          <img
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="profile icon"
+          />
+        </Link>
+      </nav>
+
+      {
+        showSearchBar && <input
+          data-testid="search-input"
+          type="search"
+          placeholder="pesquisar receita"
+        />
+      }
+    </header>
+  );
 }
+
+export default Header;
