3e2339d2fb1841a763168e7e9ffc5fa6b755b82e
ClawNarok <arthurvgf@gmail.com>
2022-05-01 08:10:50 -0300

    Padronizando requisições API

--- src/Api/foodsAPI.js
+++ src/Api/foodsAPI.js
@@ -1,14 +1,22 @@
 const ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
 export async function fetchFoods() {
-  const response = await fetch(ENDPOINT);
-  const result = await response.json();
-  return result.meals ? result.meals : [];
+  try {
+    const response = await fetch(ENDPOINT);
+    const result = await response.json();
+    return result.meals ? result.meals : [];
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function fetchFoodsSearch({ search, searchRadio }) {
-  const type = searchRadio === 'i' ? 'filter' : 'search';
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/${type}.php?${searchRadio}=${search}`);
-  const result = await response.json();
-  return result.meals ? result.meals : [];
+  try {
+    const type = searchRadio === 'i' ? 'filter' : 'search';
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/${type}.php?${searchRadio}=${search}`);
+    const result = await response.json();
+    return result.meals ? result.meals : [];
+  } catch (error) {
+    return [];
+  }
 }
