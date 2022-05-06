2f639e20c3506c9d16810e645eb4d1781a65f798
Eric Santos <eric.silvasantos44@gmail.com>
2022-05-06 12:08:54 -0300

    [Main group 4] Tela de Receitas Favoritas +Criação de componente para os cards horizontais +Lógica para mostrar as informações corretas nas telas de receitas favoritas e receitas concluidas +correção do nome dos arquivos

---
+++ src/Components/CardHorizontalRecipe.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import {
+  removeStorageFavoriteRecipes,
+  getStorageFavoriteRecipes,
+} from '../Services/localStorage';
+
+function CardHorizontalRecipe(props) {
+  const { recipe, index, doneOrFavorite, setRenderRecipes } = props;
+  console.log(recipe);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleButtonShare = (type, id) => {
+    navigator.clipboard.writeText(`http://localhost:3000/${`${type}s/${id}`}`);
+    setIsCopied((prev) => !prev);
+  };
+
+  const handleBtnFavorite = () => {
+    removeStorageFavoriteRecipes(recipe);
+    setRenderRecipes(getStorageFavoriteRecipes());
+  };
+
+  return (
+    <div>
+      <Link to={ `${recipe.type}s/${recipe.id}` }>
+        <img
+          width="100%"
+          data-testid={ `${index}-horizontal-image` }
+          src={ recipe.image }
+          alt={ recipe.name }
+        />
+      </Link>
+      { recipe.type === 'food' ? (
+        <p data-testid={ `${index}-horizontal-top-text` }>
+          {`${recipe.nationality} - ${recipe.category} `}
+        </p>
+      ) : (
+        <p data-testid={ `${index}-horizontal-top-text` }>
+          {recipe.alcoholicOrNot}
+        </p>) }
+      <button
+        type="button"
+        src={ shareIcon }
+        data-testid={ `${index}-horizontal-share-btn` }
+        onClick={ () => handleButtonShare(recipe.type, recipe.id) }
+      >
+        {isCopied ? 'Link copied!' : <img src={ shareIcon } alt="shareIcon" />}
+      </button>
+      { doneOrFavorite.includes('Favorite') && (
+        <button
+          onClick={ handleBtnFavorite }
+          data-testid={ `${index}-horizontal-favorite-btn` }
+          src={ blackHeartIcon }
+          type="button"
+        >
+          <img src={ blackHeartIcon } alt="blackHeartIcon" />
+        </button>
+      )}
+      <Link to={ `${recipe.type}s/${recipe.id}` }>
+        <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+      </Link>
+      <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+      { doneOrFavorite.includes('Done') && (
+        <p>
+          {recipe.tags.length > 0 && recipe.tags
+            .slice(0, 2).map((tag) => (
+              <span
+                key={ tag }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+              >
+                {tag}
+              </span>
+            ))}
+        </p>)}
+    </div>
+  );
+}
+
+CardHorizontalRecipe.propTypes = {
+  setRenderRecipes: PropTypes.func.isRequired,
+  doneOrFavorite: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  recipe: PropTypes.shape({
+    id: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string),
+    alcoholicOrNot: PropTypes.string,
+    doneDate: PropTypes.string,
+    nationality: PropTypes.string,
+    category: PropTypes.string,
+  }).isRequired,
+};
+
+export default CardHorizontalRecipe;
--- src/Components/ListDoneRecipes.js
+++ src/Components/ListDoneRecipes.js
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import shareIcon from '../images/shareIcon.svg';
-// import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+// import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+import RecipesContext from '../Context/RecipesContext';
+// import shareIcon from '../images/shareIcon.svg';
+import {
+  getStorageDoneRecipes,
+  getStorageFavoriteRecipes,
+} from '../Services/localStorage';
+import CardHorizontalRecipe from './CardHorizontalRecipe';
 
-function ListDoreRecipes() {
-  // const history = useHistory();
+function ListRecipesInStorege() {
+  // const { doneOrFavorite } = props;
+  // console.log(url);
+  const { title } = useContext(RecipesContext);
+  console.log(title.includes('Favorite'));
   const [doneRecipes, setDoneRecipes] = useState([]);
-  const [isCopied, setIsCopied] = useState(false);
   const [renderRecipes, setRenderRecipes] = useState([]);
 
   // pontos de melhoria:
@@ -15,13 +23,12 @@ function ListDoreRecipes() {
   // 3 - criar um aqruivo css para modificar a largura da imagem do card
 
   useEffect(() => {
-    const recipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+    const recipes = title.includes('Favorite')
+      ? getStorageFavoriteRecipes()
+      : getStorageDoneRecipes();
     setDoneRecipes(recipes);
     setRenderRecipes(recipes);
-  }, []);
-
-  // console.log(renderRecipes);
-  // console.log(doneRecipes);
+  }, [title]);
 
   const handleButtonAll = () => {
     setRenderRecipes(doneRecipes);
@@ -35,10 +42,10 @@ function ListDoreRecipes() {
     setRenderRecipes(doneRecipes.filter((recipe) => recipe.type === 'drink'));
   };
 
-  const handleButtonShare = (type, id) => {
-    navigator.clipboard.writeText(`http://localhost:3000/${`${type}s/${id}`}`);
-    setIsCopied((prev) => !prev);
-  };
+  // const handleButtonShare = (type, id) => {
+  //   navigator.clipboard.writeText(`http://localhost:3000/${`${type}s/${id}`}`);
+  //   setIsCopied((prev) => !prev);
+  // };
 
   return (
     <div>
@@ -67,50 +74,61 @@ function ListDoreRecipes() {
       </div>
       {renderRecipes.length > 0 && renderRecipes
         .map((recipe, index) => (
-          <div key={ recipe.id }>
-            <Link to={ `${recipe.type}s/${recipe.id}` }>
-              <img
-                width="100%"
-                data-testid={ `${index}-horizontal-image` }
-                src={ recipe.image }
-                alt={ recipe.name }
-              />
-            </Link>
-            { recipe.type === 'food' ? (
-              <p data-testid={ `${index}-horizontal-top-text` }>
-                {`${recipe.nationality} - ${recipe.category} `}
-              </p>
-            ) : (
-              <p data-testid={ `${index}-horizontal-top-text` }>
-                {recipe.alcoholicOrNot}
-              </p>) }
-            <button
-              type="button"
-              src={ shareIcon }
-              data-testid={ `${index}-horizontal-share-btn` }
-              onClick={ () => handleButtonShare(recipe.type, recipe.id) }
-            >
-              {isCopied ? 'Link copied!' : <img src={ shareIcon } alt="shareIcon" />}
-            </button>
-            <Link to={ `${recipe.type}s/${recipe.id}` }>
-              <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
-            </Link>
-            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-            <p>
-              {recipe.tags.length > 0 && recipe.tags
-                .slice(0, 2).map((tag) => (
-                  <span
-                    key={ tag }
-                    data-testid={ `${index}-${tag}-horizontal-tag` }
-                  >
-                    {tag}
-                  </span>
-                ))}
-            </p>
-          </div>
+          <CardHorizontalRecipe
+            key={ recipe.id }
+            recipe={ recipe }
+            index={ index }
+            doneOrFavorite={ title }
+            setRenderRecipes={ setRenderRecipes }
+          />
+          // <div key={ recipe.id }>
+          //   <Link to={ `${recipe.type}s/${recipe.id}` }>
+          //     <img
+          //       width="100%"
+          //       data-testid={ `${index}-horizontal-image` }
+          //       src={ recipe.image }
+          //       alt={ recipe.name }
+          //     />
+          //   </Link>
+          //   { recipe.type === 'food' ? (
+          //     <p data-testid={ `${index}-horizontal-top-text` }>
+          //       {`${recipe.nationality} - ${recipe.category} `}
+          //     </p>
+          //   ) : (
+          //     <p data-testid={ `${index}-horizontal-top-text` }>
+          //       {recipe.alcoholicOrNot}
+          //     </p>) }
+          //   <button
+          //     type="button"
+          //     src={ shareIcon }
+          //     data-testid={ `${index}-horizontal-share-btn` }
+          //     onClick={ () => handleButtonShare(recipe.type, recipe.id) }
+          //   >
+          //     {isCopied ? 'Link copied!' : <img src={ shareIcon } alt="shareIcon" />}
+          //   </button>
+          //   <Link to={ `${recipe.type}s/${recipe.id}` }>
+          //     <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          //   </Link>
+          //   <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+          //   <p>
+          //     {recipe.tags.length > 0 && recipe.tags
+          //       .slice(0, 2).map((tag) => (
+          //         <span
+          //           key={ tag }
+          //           data-testid={ `${index}-${tag}-horizontal-tag` }
+          //         >
+          //           {tag}
+          //         </span>
+          //       ))}
+          //   </p>
+          // </div>
         ))}
     </div>
   );
 }
 
-export default ListDoreRecipes;
+// ListRecipesInStorege.propTypes = {
+//   doneOrFavorite: PropTypes.string.isRequired,
+// };
+
+export default ListRecipesInStorege;
